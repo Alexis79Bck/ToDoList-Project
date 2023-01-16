33139345291e61e39644eac702fbc6fbc374e871
Alexis Eduardo Mata <94631808+Alexis79Bck@users.noreply.github.com>
2023-01-16 07:57:23 -0400

    continuacion

--- src/components/ToDoApp.js
+++ src/components/ToDoApp.js
@@ -3,25 +3,38 @@ import { useState } from "react";
 export default function ToDoApp () {
 
     const [title, setTitle] = useState("Hola");
+    const [toDoList, setToDoList] = useState([]);
 
-    function handleClick(e) {
-        e.preventDefault();
-        setTitle("Mundo");
-    }
+    // function handleClick(e) {
+    //     e.preventDefault();
+    //     setTitle("Mundo");
+    // }
 
     function handleChange(e) {
         const value = e.target.value;
         setTitle(value);
     }
 
+    function handleSubmit(e) {
+        e.preventDefault();
+        const newToDo = {
+            id: crypto.randomUUID(),
+            title: title,
+            complete: false        
+        }
+
+        setToDoList([...toDoList, newToDo]);
+        console.clear();
+        console.log(toDoList, newToDo);
+    }
+
     return (
     <div className="toDoContainer"> 
-        <form className="toDoCreateForm">
+        <form className="toDoCreateForm" onSubmit={handleSubmit}>
             <input className="toDoInput" onChange={handleChange} value={ title }/>
-            <input className="buttonNew" type="submit" onClick={ handleClick } value="New" />
+            <input className="buttonNew" type="submit" onSubmit={handleSubmit} value="New" /> 
         </form>
-
-        { title }
+        
     </div>
     );
 }
